6cb24e8cef317e8d414c9ba0cd9d3c8212d4bf97
Dinithi Sonali Gamage <75924676+Dini-s@users.noreply.github.com>
2025-10-01 17:36:33 +0530

    change nav bar

--- navigation/ClientReporterBottomTab.js
+++ navigation/ClientReporterBottomTab.js
@@ -1,19 +1,21 @@
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs/lib/typescript/src";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import clientHomeScreen from "../screens/clientReporter/clientHomeScreen";
 import clientReportIncident from "../screens/clientReporter/clientReportIncident";
 import myReport from "../screens/clientReporter/myReport";
 import Notification from "../screens/clientReporter/Notification";
 import ClientProfile from "../screens/clientReporter/ClientProfile";
+import React from "react";
 
 const Tab = createBottomTabNavigator();
 
 export default function ClientBottom() {
     return (
         <Tab.Navigator
+
             screenOptions={({ route }) => ({
                 tabBarIcon: ({ color, size }) => {
                     let iconName;
-                    if (route.name === 'Home') iconName = 'home';
+                    if (route.name === 'Hoe') iconName = 'home';
                     else if (route.name === 'Report') iconName = 'flag';
                     else if (route.name === 'My Reports') iconName = 'file';
                     else if (route.name === 'Notification') iconName = 'notification';
@@ -21,9 +23,10 @@ export default function ClientBottom() {
                 },
                 tabBarActiveTintColor: "#146C94",
                 tabBarInactiveTintColor: "#19A7CE",
+                tabBarShowLabel: false,
 
             })}>
-            <Tab.Screen name="Home" component={clientHomeScreen} />
+            <Tab.Screen name="Hoe" component={clientHomeScreen} />
             <Tab.Screen name="Report" component={clientReportIncident} />
             <Tab.Screen name="My Reports" component={myReport} />
             <Tab.Screen name="Notification" component={Notification} />
--- screens/clientReporter/clientHomeScreen.js
+++ screens/clientReporter/clientHomeScreen.js
@@ -4,7 +4,7 @@ import { Text, View } from 'react-native'
 export default function clientHomeScreen() {
     return (
         <View style={{ flex: 1, justifyContent: "Center", alignItems: "center" }}>
-            <Text>Client Home Screen</Text>
+            <Text>Client Reporter Home Screen</Text>
         </View>
     );
 }
